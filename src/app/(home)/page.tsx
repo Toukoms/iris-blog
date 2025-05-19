fb444081d7a6ceb61747c18d385d5c04a888ffa0
Tokiniaina <tokiniaina090605@gmail.com>
2025-05-19 09:43:32 +0300

    refactor(profile): show profile of the author from article

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,32 +1,36 @@
 import ArticleCard from "@/components/article/article-card";
+import CreateArticleButton from "@/components/article/create-article-btn";
 import { api } from "@/trpc/server";
 
 export default async function Home() {
-  const articles = await api.article.getArticles();
-  return (
-    <div>
-      <h2>Articles</h2>
-      <p>
-        Welcome to the Iris Blog! Here you can find the latest tech news and
-        articles.
-      </p>
+	const articles = await api.article.getArticles();
+	return (
+		<div>
+			<div className="mt-8 flex justify-between">
+				<div>
+					<h2 className="my-0 font-semibold text-2xl">Articles</h2>
+					<p>Here are the latest articles:</p>
+				</div>
 
-      {articles !== null && articles.length > 0 ? (
-        <div className="grid w-full grid-cols-1 gap-4">
-          {articles.map((article) => (
-            <ArticleCard
-              key={article.id}
-              {...article}
-              authorName={article.author.name || ""}
-              authorImage={article.author.image || ""}
-            />
-          ))}
-        </div>
-      ) : (
-        <p className="w-full text-center text-accent">
-          No article for the moment. Stay tuned for more updates!
-        </p>
-      )}
-    </div>
-  );
+				<CreateArticleButton />
+			</div>
+
+			{articles !== null && articles.length > 0 ? (
+				<div className="grid w-full grid-cols-1 gap-4">
+					{articles.map((article) => (
+						<ArticleCard
+							key={article.id}
+							{...article}
+							authorName={article.author.name || ""}
+							authorImage={article.author.image || ""}
+						/>
+					))}
+				</div>
+			) : (
+				<p className="w-full text-center text-accent">
+					No article for the moment. Stay tuned for more updates!
+				</p>
+			)}
+		</div>
+	);
 }
--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,29 +1,28 @@
 import ArticleCard from "@/components/article/article-card";
+import CreateArticleButton from "@/components/article/create-article-btn";
 import LogoutButton from "@/components/auth/logout-btn";
-import { auth } from "@/server/auth";
 import { api } from "@/trpc/server";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export const revalidate = 120;
 
-async function ProfilePage() {
-  const session = await auth();
-  const authenticated = session?.user !== undefined;
-  const user = session?.user;
+async function ProfilePage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const user = await api.user.getUserById(id);
 
-  if (!authenticated) {
+  if (!user) {
     redirect("/");
   }
 
-  const articles = await api.article.getUserArticles();
+  const articles = await api.article.getUserArticles(id);
 
   return (
     <div>
       <div>
         <h2 className="capitalize">Profile: information and articles</h2>
         <div className="flex items-center justify-between gap-4">
-          <div className="flex items-center gap-4">
+          <div className="flex gap-4">
             {user?.image ? (
               <Image
                 src={user?.image}
@@ -33,25 +32,29 @@ async function ProfilePage() {
                 height={640}
               />
             ) : (
-              <div className="avatar placeholder">
-                <div className="w-10 rounded-full bg-neutral text-neutral-content">
+              <div className="avatar avatar-placeholder">
+                <div className="w-32 rounded-full bg-neutral text-neutral-content">
                   <span>{user?.name?.slice(2)}</span>
                 </div>
               </div>
             )}
-            <div className="flex h-fit flex-col gap-2">
-              <h2 className="my-0 font-semibold text-xl">{user?.name}</h2>
-              <p>{user?.email}</p>
+            <div className="mt-4 flex flex-col justify-start gap-1">
+              <p className="my-0 font-semibold text-3xl">{user?.name}</p>
+              <p className="my-0 text-secondary text-sm">{user?.email}</p>
+              <LogoutButton />
             </div>
           </div>
-          <LogoutButton />
+
+          <div>
+            <CreateArticleButton />
+          </div>
         </div>
       </div>
       <div className="mt-8">
         <h2 className="font-semibold text-2xl">Articles</h2>
         <p>Here are the articles you have written:</p>
       </div>
-      <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-2">
         {articles && articles.length > 0 ? (
           articles.map((article) => (
             <ArticleCard
